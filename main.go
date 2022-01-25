69259af52c815d64657b5c38782f08681c57c6e6
Jack Mordaunt <jackmordaunt.dev@gmail.com>
2022-01-26 00:21:30 +0800

    webp: upgrade deps
    Bump libc to include bsearch and regenerate libwebp.
    
    Signed-off-by: Jack Mordaunt <jackmordaunt.dev@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
 	"image"
 	"os"
@@ -43,9 +42,8 @@ func run(args []string) error {
 		}
 	}
 	var (
-		quality = float32(1.0)
-		// BUG: turning this off leads to divide by zero.
-		lossless = int32(1)
+		quality  = float32(100.0)
+		lossless = int32(0)
 		// out buffer to contain webp data.
 		out *byte = nil
 	)
@@ -80,47 +78,4 @@ func run(args []string) error {
 		return fmt.Errorf("writing out webp image: %w", err)
 	}
 	return nil
-}
-
-// bsearch stub that ostensibly implements a binary search on unsafe data.
-//
-// NOTE(jfm): current implementation is actually just a linear search
-// that ignores the predicate function for now.
-//
-// Input data is expected to be sorted low to high, and contain i32.
-//
-// Assuming i32 is technically eroneous and we should use the predicate
-// function to capture type information to keep this function data agnostic.
-// However it might be better to simply implement a Go idiomatic function
-// that doesn't operate on raw memory.
-//
-// Given that this function is used exactly once and the data used is i32
-// this works.
-//
-//       key: raw pointer to object that serves as key
-//      base: raw pointer to first element in array
-//  elements: number of elements in array
-//      size: size of each element in bytes
-// predicate: comparison function applied to each set of candidates [type predicate func(left, right uintptr) int32]
-//
-func bsearch(tls *libc.TLS, key, base uintptr, elements, size uint64, predicate uintptr) uintptr {
-	var (
-		// This conversion from uintptr to unsafe.Pointer may actually be valid based
-		// on the codebase using uintptr exclusively, the memory is not GC'd like standard Go
-		// but rather allocated with `libc.Alloc`.
-		k    int32 = *(*int32)(unsafe.Pointer(key))
-		data       = libc.GoBytes(base, int(elements*size))
-	)
-	// Linear search for exact match.
-	for ii := uint64(0); ii < uint64(len(data)); ii += size {
-		n := int32(binary.LittleEndian.Uint32(data[ii : ii+size]))
-		if n == k {
-			// return the base pointer offset by the number of bytes
-			// at which the value exists.
-			return base + uintptr(ii)
-		}
-	}
-	// return the base memory offset if we didn't find a match. May not
-	// be correct semantically, but should avoid memory corruption.
-	return base
 }
