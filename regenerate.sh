e90b46f14d96460408f122499b2a8a78ca1e9032
Chris Waldon <christopher.waldon.dev@gmail.com>
2022-01-25 11:55:26 -0500

    feat: enable regenerating the Go code with go generate
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

---
+++ regenerate.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -eux
+
+cd ./c-lib/
+make distclean
+./autogen.sh
+./configure \
+    --disable-libwebpmux \
+    --disable-libwebpdemux \
+    --disable-sse4.1 \
+    --disable-sse2 \
+    --disable-neon \
+    --disable-neon-rtcd \
+    --disable-threading \
+    --disable-gl \
+    --disable-sdl \
+    --disable-png \
+    --disable-jpeg \
+    --disable-tiff  \
+    --disable-gif  \
+    --disable-wic \
+    --disable-shared
+ccgo -compiledb cdb.json make
+CC=$(which gcc) ccgo -pkgname main -trace-translation-units -o ../libwebp.go cdb.json src/.libs/libwebp.a
+gofmt -s -w ../libwebp.go
