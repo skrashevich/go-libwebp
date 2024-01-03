82741fa8693af21b7c819edd160275b50302b60e
Jack Mordaunt <jackmordaunt.dev@gmail.com>
2024-01-03 16:37:13 +0800

    webp: use webp decoder
    Instead of the stdlib decoder.
    
    Signed-off-by: Jack Mordaunt <jackmordaunt.dev@gmail.com>

--- webp/decode.go
+++ webp/decode.go
@@ -1,15 +1,21 @@
 package webp
 
 import (
+	"fmt"
 	"image"
 	"io"
 
+	transpiled "git.sr.ht/~jackmordaunt/go-libwebp/lib/transpiled/webp"
 	stdwebp "golang.org/x/image/webp"
 )
 
 // Decode reads a WEBP image from r and returns it as an image.Image.
 func Decode(r io.Reader) (image.Image, error) {
-	return stdwebp.Decode(r)
+	by, err := io.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("buffering data: %w", err)
+	}
+	return transpiled.DecodeImpl(by)
 }
 
 // DecodeConfig returns the color model and dimensions of a WEBP image without
